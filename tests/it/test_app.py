f8bd7170d5e58586b1d922c3052034f5d752f3fa
Ilaletdinov Almaz <45946541+blablatdinov@users.noreply.github.com>
2026-08-15 19:37:29 +0300

    Update repo (#64)

--- tests/it/test_app.py
+++ tests/it/test_app.py
@@ -1,24 +1,5 @@
-# The MIT License (MIT).
-#
-# Copyright (c) 2024 Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
-# OR OTHER DEALINGS IN THE SOFTWARE.
+# SPDX-FileCopyrightText: Copyright (c) 2024-2026 Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
+# SPDX-License-Identifier: MIT
 
 # flake8: noqa: S . Not a production code
 
@@ -50,7 +31,7 @@ def _test_dir(tmpdir_factory: TempdirFactory, current_dir: str) -> Generator[Non
 
 @pytest.mark.usefixtures('_test_dir')
 @pytest.mark.parametrize('version', [
-    ('flake8==5.0.0',),
+    ('flake8==7.1.0',),
     ('flake8', '-U'),
 ])
 def test_dependency_versions(version: tuple[str]) -> None:
